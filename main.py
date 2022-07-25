228370317c0970034834fa667af67c0d1739eabc
Marvish <mschandr@uci.edu>
2022-07-25 13:04:46 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,49 @@
+# Author: Marvish Chandra
+'''This program is intended to help a person or couple decide to eat from the most popular fast food places
+based on the most bought food items there. '''
+
+
+def mostPopular(fastSpots):
+    fastSpots = ["McDonalds", "Burger King", "Jack in the Box", "Taco Bell", "In-N-Out","Chick-fil-A"]
+    print(fastSpots)
+    var myChoice = input("Which popular fast food restaurant would you like to eat from?")
+    print(myChoice)
+
+def favoriteDonalds(favoriteDon):
+    favoriteDon = ["Big Mac", "Chicken McNuggets", "French Fries"]
+    print(favoriteDon)
+    var guiltyDon = input("What would you like to eat from McDonalds?")
+    print(guiltyDon)
+
+def favoriteBurger(favoriteKing):
+    favoriteKing = ["The Whopper", "Original Chicken Sandwich", "French Fries"]
+    print(favoriteKing)
+    var noOptions = input("What would you like to eat from Burger King?")
+    print(noOptions)
+
+def favoriteBox(favoriteJack):
+    favoriteJack = ["Bacon Ultimate Cheeseburger", "Sourdough Jack", "Loaded Tiny Tacos", "Curly Fries"]
+    print(favoriteJack)
+    var heavyOption = input("What would you like to eat from Jack in the Box?")
+    print(heavyOption)
+
+def favoriteTaco(favoriteBell):
+    favoriteBell = ["Crunchwrap Supreme", "Mexican Pizza", "Chalupas", "Cheesy Gordita Crunch"]
+    print(favoriteBell)
+    var cheapMex = input("What would you like to eat from Taco Bell?")
+    print(cheapMex)
+
+def favoriteAnimal(favoriteOut):
+    favoriteOut = ["Animal Style Hamburger", "The Double Double", "The Four for Four", "Animal Style Fries",
+               "Neapolitan Shake"]
+    print(favoriteOut)
+    var favoriteHam = input("What would you like to eat from In-N-Out?")
+    print(favoriteHam)
+
+def favoriteFast(favoriteChick):
+    favoriteChick = ["Chick-fil-A Sandwich","Waffle Potato Fries","Chick-fil-A Nuggets"]
+    print(favoriteChick)
+    var feelingHealthy = input("What would you like to eat from Chick-fil-A?")
+    print(feelingHealthy)
+
+
